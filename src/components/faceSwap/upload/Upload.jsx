5c15314e0e4204a9ed300f7f4968a5e8a87f845d
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-03-07 15:39:55 +0530

    update upload image

--- src/components/faceSwap/upload/Upload.jsx
+++ src/components/faceSwap/upload/Upload.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styles from "./upload.module.css";
+import upload from "./../../../assets/faceSwap/upload/upload.png";
+import uploadBtn from "./../../../assets/faceSwap/upload/uploadBtn.png";
+import captureBtn from "./../../../assets/faceSwap/upload/captureBtn.png";
 
 export default function Upload() {
+  const uploadTemplateRef = useRef(null);
+  const uploadPhotoRef = useRef(null);
+  const uploadCaptureRef = useRef(null);
+  const [uploadTemplateImg, setUploadTemplateImg] = useState("");
+  const [uploadPhotoImg, setUploadPhotoImg] = useState("");
+
+  // upload template
+  const handleUploadTemplateChange = e => {
+    console.log(e.target.files[0]);
+    setUploadTemplateImg(e.target.files[0]);
+  };
+  const handleUploadTemplate = () => {
+    console.log("uploading template");
+    uploadTemplateRef.current.click();
+  };
+
+  // upload photo
+  const handleUploadPhotoChange = e => {
+    console.log(e.target.files[0]);
+    setUploadPhotoImg(e.target.files[0]);
+  };
+  const handleUploadPhoto = () => {
+    console.log("uploading photo");
+    uploadPhotoRef.current.click();
+  };
   return (
     <div className={styles.Upload}>
       {/* credit btn */}
@@ -14,16 +42,82 @@ export default function Upload() {
           <p className={`grd-txt ${styles.title}`}>
             Upload or Choose
             <br />
-            our Templates
+            Our Templates
           </p>
           <div className={styles.imgContainer}>
             <div className={styles.imgParent}>
-              <img src="#" alt="upload template" />
+              <img
+                src={
+                  uploadTemplateImg
+                    ? URL.createObjectURL(uploadTemplateImg)
+                    : upload
+                }
+                alt="upload template"
+              />
             </div>
-            <div className={styles.uploadBtnContainer}></div>
+
+            <div className={styles.uploadBtnContainer}>
+              <div className={styles.btnGrandParent}>
+                <div
+                  className={styles.btnParent}
+                  onClick={handleUploadTemplate}
+                >
+                  <img src={uploadBtn} alt="upload button" />
+                  <input
+                    type="file"
+                    ref={uploadTemplateRef}
+                    onChange={handleUploadTemplateChange}
+                    style={{ display: "none" }}
+                  />
+                </div>
+                <p>Upload</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* upload/capture your photo */}
+        <div className={styles.uploadYourPhoto}>
+          <p className={`grd-txt ${styles.title}`}>
+            Upload or Capture
+            <br />
+            Your Photo
+          </p>
+          <div className={styles.imgContainer}>
+            <div className={styles.imgParent}>
+              <img
+                src={
+                  uploadPhotoImg ? URL.createObjectURL(uploadPhotoImg) : upload
+                }
+                alt="upload template"
+              />
+            </div>
+            <div className={`${styles.uploadBtnContainer}`}>
+              <div className={styles.btnGrandParent}>
+                <div onClick={handleUploadPhoto} className={styles.btnParent}>
+                  <img src={uploadBtn} alt="upload button" />
+                  <input
+                    type="file"
+                    ref={uploadPhotoRef}
+                    onChange={handleUploadPhotoChange}
+                    style={{ display: "none" }}
+                  />
+                </div>
+                <p>Upload</p>
+              </div>
+              <div className={styles.btnGrandParent}>
+                <div className={styles.btnParent}>
+                  <img src={captureBtn} alt="capture button" />
+                </div>
+                <p>Capture</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
+      {/* swap now btn */}
+      <button className={`btn2 ${styles.swapNowBtn}`}>Swap Now</button>
     </div>
   );
 }
